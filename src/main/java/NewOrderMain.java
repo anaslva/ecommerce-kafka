47c844d147a2f0d4445567f3af698454a8cbb15f
anaslva <anacarolina.slva@hotmail.com>
2023-07-20 19:10:36 -0300

    [feat] add loop for more messages and alter messages value

--- src/main/java/NewOrderMain.java
+++ src/main/java/NewOrderMain.java
@@ -6,12 +6,15 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
         try(var dispatcher = new KafkaDispatcher()){
 
-            var key = UUID.randomUUID().toString();
-            var value = key + "1, 12467, 500";
-            dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+            for(int i=0; i<= 10; i++){
+                var key = UUID.randomUUID().toString();
+                var value = key + " " + i + " " + 12467+1 + " " + 500+i ;
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
 
-            var email = "Thanks for ordering. Your order are being processed";
-            dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                var email = "Thanks for ordering. Your order are being processed";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+            }
+
         }
 
 
