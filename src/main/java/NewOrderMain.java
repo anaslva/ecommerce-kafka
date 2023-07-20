e1c08e11fc77c74c7667e9c942a150bbb8694b64
anaslva <anacarolina.slva@hotmail.com>
2023-07-20 14:03:32 -0300

    add email service and log service with consumers

--- src/main/java/NewOrderMain.java
+++ src/main/java/NewOrderMain.java
@@ -1,3 +1,4 @@
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -10,14 +11,18 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "1, 12467, 500";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex)-> {
-            if(ex != null){
+        var productRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", value, value);
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/offset " + data.offset() + "/timestamp " + data.timestamp());
-        }).get();
+        };
+
+        producer.send(productRecord, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     public static Properties properties(){
